bcf20849ac7096cbb8a747cc996973934cb1350f
Øyvind Malin <oyvindmal@outlook.com>
2024-04-25 23:29:04 +0200

    All about the base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	export let data;
+	console.log(data);
+  </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1">Øyvind Malin</h1>
-		
+		<ul>
+			{#each data.albums as album}
+			<li>{album.title}</li>
+		  {/each}
+		</ul>
 	</div>
 </div>
