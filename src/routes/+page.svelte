28eb1d8aed2f9a0917aeaa3b5673f58337ff0603
Øyvind Malin <oyvindmal@outlook.com>
2024-05-21 00:19:09 +0200

    fixing background bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,14 +15,14 @@
 	</section>
 </div>
 <div class="grid grid-cols-2 gap-4">
-	<div style="height:70vh;" class="card bg-center p-4 text-center content-center align-middle bg-[url('https://photos.oyvindmal.no/medium/valo24-honningbarna/honningbarna-7.jpg')]">
+	<div style="height:70vh;" class="card bg-center bg-cover p-4 text-center content-center align-middle bg-[url('https://photos.oyvindmal.no/medium/valo24-honningbarna/honningbarna-7.jpg')]">
 
 		<h2 class="h1 text-neutral-50"><a href="/events">Events</a></h2>
 	</div>
 	
 	
 	
-	<div  style="height:70vh;" class="card bg-center p-4 text-center content-center bg-[url('https://photos.oyvindmal.no/medium/190524orn/IMG_0455.JPG')]">
+	<div  style="height:70vh;" class="card bg-center bg-cover p-4 text-center content-center bg-[url('https://photos.oyvindmal.no/medium/190524orn/IMG_0455.JPG')]">
 	
 		<h2 class="h1 text-neutral-50"><a href="/album">Alle album</a></h2>
 	</div>
