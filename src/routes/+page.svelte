1b9384e7def1067b5cf87f7e1a50ef217371721c
Øyvind Malin <oyvindmal@outlook.com>
2024-05-21 00:24:58 +0200

    design stuffs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,14 +17,14 @@
 <div class="grid grid-cols-2 gap-4">
 	<div style="height:70vh;" class="card bg-center bg-cover p-4 text-center content-center align-middle bg-[url('https://photos.oyvindmal.no/medium/valo24-honningbarna/honningbarna-7.jpg')]">
 
-		<h2 class="h1 text-neutral-50"><a href="/events">Events</a></h2>
+		<h2 class="h1 text-neutral-50 bg-slate-500 p-4 rounded-2xl  opacity-90"><a href="/events">Konserter og events &rsaquo;</a></h2>
 	</div>
 	
 	
 	
 	<div  style="height:70vh;" class="card bg-center bg-cover p-4 text-center content-center bg-[url('https://photos.oyvindmal.no/medium/190524orn/IMG_0455.JPG')]">
 	
-		<h2 class="h1 text-neutral-50"><a href="/album">Alle album</a></h2>
+		<h2 class="h1 text-neutral-50 bg-slate-500 p-4 rounded-2xl  opacity-90"><a href="/album">Alle album &rsaquo;</a></h2>
 	</div>
 	
 </div>
