5c3dd7ff75cd6f9b4b5023d9627edd9a85b2127d
Øyvind Malin <oyvindmal@outlook.com>
2024-05-21 00:04:23 +0200

    Super basic front

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,15 @@
 	export let data;
 	console.log(data);
   </script>
-<p>Not yet</p>
+<div style="height:70vh;" class="card  p-4 text-center content-center align-middle bg-[url('https://photos.oyvindmal.no/medium/valo24-honningbarna/honningbarna-7.jpg')]">
+
+	<h2 class="h1 text-neutral-50"><a href="/events">Events</a></h2>
+</div>
 
 
+
+<div  style="height:70vh;" class="card p-4 text-center content-center bg-[url('https://photos.oyvindmal.no/medium/190524orn/IMG_0455.JPG')]">
+
+	<h2 class="h1 text-neutral-50"><a href="/album">Alle album</a></h2>
+</div>
 
