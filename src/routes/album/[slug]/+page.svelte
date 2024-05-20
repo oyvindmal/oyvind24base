7258253a3edcdf96ae3e5fa01ece20c0a1fe8577
Øyvind Malin <oyvindmal@outlook.com>
2024-05-20 23:50:38 +0200

    Very basic opplegg

--- src/routes/album/[slug]/+page.svelte
+++ src/routes/album/[slug]/+page.svelte
@@ -1,82 +1,70 @@
-<script>
-      import { onMount } from 'svelte';
+<script lang="ts">
     /** @type {import('./$types').PageData} */
     export let data;
-    let albumContainer;
-
-const handleThumbClick = (id) => {
-    console.log(id);
-
-    let selectedImage = "image"+id;
-    console.log(selectedImage);
-
-    // Get all child div elements with the id 'albumImages'
-    const albumImages = albumContainer.querySelectorAll('.albumImage');
-    console.log(albumImages)
-    // Loop through all children and set display to none
-    albumImages.forEach(image => {
-        console.log(image.id);
-        image.style.display = 'none';
-     
-    });
-
-    // Find and display the element with the matching id
-    const targetImage = albumContainer.querySelector(`.albumImage[id="${selectedImage}"]`);
-    console.log(targetImage);
-        if (targetImage) {
-            console.log(`Showing image with id: ${selectedImage}`);
-            targetImage.style.display = 'block';
-        } else {
-            console.log(`No image found with id: ${selectedImage}`);
-        }
-};
 
 
-onMount(() => {
-        if (albumContainer) {
-            // Get all child div elements with the class 'albumImage'
-            const albumImages = albumContainer.querySelectorAll('.albumImage');
-            // Loop through all children and set display to none except for the first one
-            albumImages.forEach((image, index) => {
-                if (index === 0) {
-                    image.style.display = 'block'; // Show the first image
-                } else {
-                    image.style.display = 'none'; // Hide all other images
-                }
-            });
-        }
-    });
+    let elemCarousel: HTMLDivElement;
+
+
+function carouselLeft(): void {
+	const x =
+		elemCarousel.scrollLeft === 0
+			? elemCarousel.clientWidth * elemCarousel.childElementCount // loop
+			: elemCarousel.scrollLeft - elemCarousel.clientWidth; // step left
+	elemCarousel.scroll(x, 0);
+}
+		
+
+function carouselRight(): void {
+	const x =
+		elemCarousel.scrollLeft === elemCarousel.scrollWidth - elemCarousel.clientWidth
+			? 0 // loop
+			: elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
+	elemCarousel.scroll(x, 0);
+}
+
+
+function carouselThumbnail(index: number) {
+	elemCarousel.scroll(elemCarousel.clientWidth * index, 0);
+}
+				
+								
 </script>
-<div class="container">
-        <div class="card">
-            <header class="card-header p-5"><h2 class="h2">{data.albums[0].title}</h2></header>
-            <section class="flex flex-row gap-2">
-                {#each data.albums[0].Metadata as metadata}
-                <span class="badge variant-filled">{metadata.value}</span>
-                {/each}
-            </section>
-        </div>    
-        
-    <div class="card">
-    
-        <section class="p-4">
-            <div class="grid grid-cols-1 gap-4" id="albumImages" bind:this={albumContainer}>
-                {#each data.albums[0].Photos as photo}
-            <div class="albumImage" id="image{photo.id}">
-                <img loading="lazy" class="h-auto max-w-full rounded-lg" src="https://photos.oyvindmal.no/medium/{data.albums[0].folderName}/{photo.fileName}" alt="{photo.fileName}">
-            </div>
-            {/each}
-            </div>
-        </section>
-    </div>
-    <div class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-6 py-10">
-        {#each data.albums[0].Photos as photo}
-            <div class="snap-start shrink-0 card py-10 text-center">  <img  on:click={handleThumbClick(photo.id)} loading="lazy" class="h-auto max-w-full rounded-lg" src="https://photos.oyvindmal.no/thumbs/{data.albums[0].folderName}/{photo.fileName}" alt="{photo.fileName}"></div>
-        {/each}
-    </div>
-    
-    
-    
+
+
+<div class="card p-4 grid grid-cols-[auto_1fr_auto] gap-4 items-center">
+	<!-- Button: Left -->
+	<button type="button" class="btn-icon variant-filled" on:click={carouselLeft}>
+		&larr;
+	</button>
+	<!-- Full Images -->
+	<div bind:this={elemCarousel} class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto">
+		{#each data.albums[0].Photos as photo}
+			<img
+				class="snap-center w-full rounded-container-token"
+                style="height:70vh"
+                src="https://photos.oyvindmal.no/medium/{data.albums[0].folderName}/{photo.fileName}" alt="{photo.fileName}"
+				loading="lazy"
+			/>
+		{/each}
+	</div>
+	<!-- Button: Right -->
+	<button type="button" class="btn-icon variant-filled" on:click={carouselRight}>
+	    &rarr;
+	</button>
 </div>
 
 
+<div class="card p-4 grid grid-cols-6 gap-4">
+	{#each data.albums[0].Photos as photo, i}
+		<button type="button" on:click={() => carouselThumbnail(i)}>
+			<img
+				class="rounded-container-token"
+				src="https://photos.oyvindmal.no/thumbs/{data.albums[0].folderName}/{photo.fileName}" 
+           
+				alt={photo.fileName}
+				loading="lazy"
+			/>
+		</button>
+	{/each}
+</div>
