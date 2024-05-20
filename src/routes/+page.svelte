8009b7679682e6d4396d72d73b64be0c2423234a
Øyvind Malin <oyvindmal@outlook.com>
2024-05-21 00:16:46 +0200

    Final.. Release candidate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,15 +14,17 @@
 		<p class="text-neutral-50 p-4"> Mitt navn er Øyvind, jeg tar bilder av ting.</p>
 	</section>
 </div>
-<div style="height:70vh;" class="card  p-4 text-center content-center align-middle bg-[url('https://photos.oyvindmal.no/medium/valo24-honningbarna/honningbarna-7.jpg')]">
+<div class="grid grid-cols-2 gap-4">
+	<div style="height:70vh;" class="card bg-center p-4 text-center content-center align-middle bg-[url('https://photos.oyvindmal.no/medium/valo24-honningbarna/honningbarna-7.jpg')]">
 
-	<h2 class="h1 text-neutral-50"><a href="/events">Events</a></h2>
+		<h2 class="h1 text-neutral-50"><a href="/events">Events</a></h2>
+	</div>
+	
+	
+	
+	<div  style="height:70vh;" class="card bg-center p-4 text-center content-center bg-[url('https://photos.oyvindmal.no/medium/190524orn/IMG_0455.JPG')]">
+	
+		<h2 class="h1 text-neutral-50"><a href="/album">Alle album</a></h2>
+	</div>
+	
 </div>
-
-
-
-<div  style="height:70vh;" class="card p-4 text-center content-center bg-[url('https://photos.oyvindmal.no/medium/190524orn/IMG_0455.JPG')]">
-
-	<h2 class="h1 text-neutral-50"><a href="/album">Alle album</a></h2>
-</div>
-
