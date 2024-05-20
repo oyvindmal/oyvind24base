4bb0f02f9f3e0bc6178e8997524256d249ab95b4
Øyvind Malin <oyvindmal@outlook.com>
2024-05-20 23:16:59 +0200

    Albumslisting

---
+++ src/routes/album/+page.server.js
@@ -0,0 +1,21 @@
+
+import { supabase } from "$lib/supabaseClient";
+
+export async function load() {
+  const { data, error } = await supabase
+    .from('Albums')
+    .select(`
+      *,
+     
+      Metadata(*),
+      AlbumPhotos:Photos!Photos_album_id_fkey(fileName),
+      FeaturedPhoto:Photos!featuredPhotoId(fileName)
+   
+    `).eq('published', true)
+    .order('date', { ascending: false });
+    if (error) console.log('Supabase error:', error);
+
+  return {
+    albums: data ?? [],
+  };
+}
