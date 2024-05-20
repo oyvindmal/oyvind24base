50aaf4db2d31ccc359ace10ed906e346b095a078
Øyvind Malin <oyvindmal@outlook.com>
2024-05-21 00:13:28 +0200

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,18 @@
 	export let data;
 	console.log(data);
   </script>
+
+
+<div class="card bg-gradient-to-r from-gray-400 to-gray-500 p-4 text-center content-center align-middle">
+
+	<section class="p-4">
+		<h2 class="h1 text-neutral-50">Hei</h2>
+	
+	</section>
+	<section class="p-4">
+		<p class="text-neutral-50 p-4"> Mitt navn er Øyvind, jeg tar bilder av ting.</p>
+	</section>
+</div>
 <div style="height:70vh;" class="card  p-4 text-center content-center align-middle bg-[url('https://photos.oyvindmal.no/medium/valo24-honningbarna/honningbarna-7.jpg')]">
 
 	<h2 class="h1 text-neutral-50"><a href="/events">Events</a></h2>
