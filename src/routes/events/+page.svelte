17b0624bfcb5ac3642dda1f79f608c6d4472b04f
Øyvind Malin <oyvindmal@outlook.com>
2024-05-21 00:07:54 +0200

    Very basic event layout

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -3,8 +3,17 @@
     export let data;
     console.log(data);
 </script>
-<ul>
+
     {#each data.events as event}
-    <li><a href="/events/{event.id}">{event.name}</a></li>
+    <div class="card p-4">
+        <section class="p-4"><h2 class="h2"><a href="/events/{event.id}">{event.name}</a></h2></section>
+        <section class="p-4"><p>{event.description}</p></section>
+        <section class="p-4"><a href="/events/{event.id}" class="btn btn-sm variant-filled">Se innhold</a></section>
+        
+     
+
+    
+    </div>
     {/each}
-</ul>
+
+
