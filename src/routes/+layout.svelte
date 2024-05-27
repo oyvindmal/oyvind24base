f6a7bd22a3d3fd651311db3ddaebbf506dee6e75
Øyvind Malin <oyvindmal@outlook.com>
2024-05-28 00:36:24 +0200

    Basic layout for front

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,10 +9,11 @@
 	<h1 class="h2"><a href="/">Øyvind Malin</a></h1>
 	<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
 </AppBar>
-<div class="container mx-auto">
+<div  style="background:hotpink;" class="min-h-screen w-full">
 
-	<div class="grid grid-cols-1 p-4 gap-4">
-		<slot />
-	</div>
-	
+
+	<p>hurra</p>
 </div>
+
+<slot />
+
