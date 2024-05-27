f90757e9fbe4ac8e381110532b2635bd3249eb0d
Øyvind Malin <oyvindmal@outlook.com>
2024-05-27 23:03:46 +0200

    vcard

---
+++ src/routes/vcard/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+<div class="p4">
+    <h2 class="h2">Visittkort</h2>
+    <p>Her skulle det vært en qrkode</p>
+</div>
