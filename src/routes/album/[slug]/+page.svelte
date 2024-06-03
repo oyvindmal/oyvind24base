5000b62cd2b43843039d42f632481857a822e036
Øyvind Malin <oyvindmal@outlook.com>
2024-06-03 22:26:01 +0200

    Responsive

--- src/routes/album/[slug]/+page.svelte
+++ src/routes/album/[slug]/+page.svelte
@@ -7,13 +7,13 @@
     <section class="p-4">
         <h1 class="h1">{data.albums[0].title}</h1>
     </section>
-    <div class="columns-2 md:columns-3 lg:columns-4">
+    <div class="columns-1 md:columns-3 lg:columns-4">
         
     
         {#each data.albums[0].Photos as photo}
     
         <img
-            class=""
+    
            
             src="https://photos.oyvindmal.no/medium/{data.albums[0].folderName}/{photo.fileName}" alt="{photo.fileName}"
             loading="lazy"
