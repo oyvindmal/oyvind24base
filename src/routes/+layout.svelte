edf75f3ca6b6909fb1a57ddcbcda950510f0fdff
Øyvind Malin <oyvindmal@outlook.com>
2024-07-10 00:17:42 +0200

    Kontakt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 	<h1 class="h2"><a href="/">Øyvind Malin</a></h1>
 	<svelte:fragment slot="trail">
 		<a  class="btn btn-sm variant-filled" href="/albums">Alle album</a>	
+		<a  class="btn btn-sm variant-filled" href="/vcard">Kontakt</a>	
 	<LightSwitch /></svelte:fragment>
 </AppBar>
 
--- src/routes/vcard/+page.svelte
+++ src/routes/vcard/+page.svelte
@@ -2,7 +2,16 @@
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
-<div class="p4">
-    <h2 class="h2">Visittkort</h2>
-    <p>Her skulle det vært en qrkode</p>
-</div>
+<div  class="min-h-screen w-full bg-surface-700">
+	<div class=" p-4 text-center content-center align-middle">
+
+		<section class="p-4">
+			<h2 class="h1 text-neutral-50">Kontaktinfo</h2>
+		
+		</section>
+		<section class="p-4">
+			<p class="text-neutral-50 p-4"> Har jeg tatt bilde ett bilde du har lyst til å bruke. Ta kontakt på <a href="mailto:oyvind@9800.no">oyvind@9800.no</a> så blir vi enige.</p>
+		</section>
+	
+	</div>
+    </div>
