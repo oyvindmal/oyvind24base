3ab2d3c95fd72394817624e15190529530b8a69c
Øyvind Malin <oyvindmal@outlook.com>
2024-07-10 00:15:32 +0200

    Very basic nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,11 @@
 </script>
 <AppBar>
 	<svelte:fragment slot="lead">&nbsp;</svelte:fragment>
+	
 	<h1 class="h2"><a href="/">Øyvind Malin</a></h1>
-	<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+	<svelte:fragment slot="trail">
+		<a  class="btn btn-sm variant-filled" href="/albums">Alle album</a>	
+	<LightSwitch /></svelte:fragment>
 </AppBar>
 
 <slot />
