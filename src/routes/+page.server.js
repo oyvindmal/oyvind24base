99f34727152e97c57f56e7b0d70bf9cd41767c4b
Øyvind Malin <oyvindmal@outlook.com>
2024-07-09 23:42:02 +0200

    new front

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,19 @@
 
+
+
+
 import { supabase } from "$lib/supabaseClient";
 
+export async function load({params}) {
 
+  const { data: pagesData } = await supabase
+  .from("Collections")
+  .select('*,FeaturedPhoto:Photos!featuredPhotoId(fileName,Albums!album_id(folderName))')
+  .eq('published', true)
+  .order('endDate', { ascending: false });
+
+  return {
+    collections: pagesData ?? [],
+
+  };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	export let data;
-	console.log(data);
-	import SocialIcons from '@rodneylab/svelte-social-icons';
+	console.log("front",data);
   </script>
 
 <div  class="min-h-screen w-full bg-surface-700">
@@ -14,32 +13,33 @@
 		<section class="p-4">
 			<p class="text-neutral-50 p-4"> Mitt navn er Øyvind, jeg tar bilder av ting.</p>
 		</section>
-		<section class="p-4">
-			<SocialIcons alt="" network="instagram" fgColor="#eeeeee" bgColor="#111111" />
-			<SocialIcons alt="" network="tiktok" fgColor="#eeeeee" bgColor="#111111" />
-			<SocialIcons alt="" network="youtube" fgColor="#eeeeee" bgColor="#111111" />
-		</section>
-	</div>
-	<div class="grid grid-cols-1 p-4 md:grid-cols-2 gap-4">
-		<div style="height:40vh;" class=" bg-center bg-cover p-4 text-center content-center align-middle bg-[url('https://photos.oyvindmal.no/medium/valo24-honningbarna/honningbarna-7.jpg')]">
 	
-			<h2 class="h1 text-neutral-50 bg-slate-500 p-4 rounded-2xl  opacity-90"><a href="/collections/type/event">Konserter og events &rsaquo;</a></h2>
+	</div>
+	<div class="grid grid-cols-1 p-4 gap-4">
+		{#each data.collections as collection}
+		<div style="background:#FFF;" class="grid grid-cols-1 p-4 md:grid-cols-2 gap-4">
+			
+				<div>
+
+					{#if collection && collection.FeaturedPhoto}
+						
+						
+
+						<img loading="lazy" src="https://photos.oyvindmal.no/medium/{collection.FeaturedPhoto.Albums.folderName}/{collection.FeaturedPhoto.fileName}" alt="{collection.FeaturedPhoto.fileName}" srcset="">
+					{:else}
+					<img src="https://placehold.co/600x400" alt="">
+					{/if}
+		
+				</div>
+				<div>
+					<h1 class="h1"><a href="/collections/show/{collection.id}">{collection.name}</a></h1>
+					{collection.description}
+				</div>
+				
+			
 		</div>
-		
-		
-		
-		<div  style="height:40vh;" class=" bg-center bg-cover p-4 text-center content-center bg-[url('https://photos.oyvindmal.no/medium/190524orn/IMG_0455.JPG')]">
-		
-			<h2 class="h1 text-neutral-50 bg-slate-500 p-4 rounded-2xl  opacity-90"><a href="/album">Alle album &rsaquo;</a></h2>
-		</div>
-		<div  style="height:40vh; display:none;" class=" bg-center bg-cover p-4 text-center content-center bg-[url('https://photos.oyvindmal.no/medium/190524orn/IMG_0455.JPG')]">
-		
-			<h2 class="h1 text-neutral-50 bg-slate-500 p-4 rounded-2xl  opacity-90"><a href="/album">Alle album &rsaquo;</a></h2>
-		</div>
-		<div  style="height:40vh;  display:none;" class=" bg-center bg-cover p-4 text-center content-center bg-[url('https://photos.oyvindmal.no/medium/190524orn/IMG_0455.JPG')]">
-		
-			<h2 class="h1 text-neutral-50 bg-slate-500 p-4 rounded-2xl  opacity-90"><a href="/album">Alle album &rsaquo;</a></h2>
-		</div>
+
+		{/each}
 	</div>
 </div>
 
